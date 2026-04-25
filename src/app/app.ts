d4086c7e657ffc36fde98197803d8a5872e2064b
RmNLcN <romainluciano.cda.pro@gmail.com>
2026-04-25 21:36:40 +0200

    Add features to Recipe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Recipe } from './recipe/recipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Recipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
