f7bc4c8b4d149577cbd3c3596b303237b56d32a6
RmNLcN <romainluciano.cda.pro@gmail.com>
2026-04-27 16:10:00 +0200

    Add recipes datas to the Recipe component

--- src/app/recipe/recipe.ts
+++ src/app/recipe/recipe.ts
@@ -11,14 +11,127 @@ import { RecipeTypeInfos } from '../recipetype';
 export class Recipe {
   readonly baseUrl: string = `pictures`;
 
-  recipeType: RecipeTypeInfos = {
-    id: 1,
-    name: 'Apple Pie',
-    nbPerson: 4,
-    time: 30,
-    photo: `${this.baseUrl}/apple-pie.jpg`,
-    category: 'Dessert',
-    vegan: true,
-    instructions: 'Lorem Ispum... '
-  };
+  recipeTypeList: RecipeTypeInfos[] = 
+  [
+    {
+      id: 0,
+      name: 'Apple Pie',
+      nbPerson: 4,
+      time: 30,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Dessert',
+      vegan: true,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 1,
+      name: 'Cherry Pie',
+      nbPerson: 4,
+      time: 60,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Dessert',
+      vegan: true,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 2,
+      name: 'Niçoise salad',
+      nbPerson: 6,
+      time: 20,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Starter',
+      vegan: false,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 3,
+      name: 'Chicken curry',
+      nbPerson: 6,
+      time: 45,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Course',
+      vegan: false,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 4,
+      name: 'Mixed vegetables',
+      nbPerson: 4,
+      time: 30,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Course',
+      vegan: true,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 5,
+      name: 'Lasagna',
+      nbPerson: 8,
+      time: 45,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Course',
+      vegan: false,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 6,
+      name: 'Pissaladière',
+      nbPerson: 8,
+      time: 50,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Starter',
+      vegan: false,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 7,
+      name: 'Tapenade',
+      nbPerson: 6,
+      time: 45,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Starter',
+      vegan: true,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 8,
+      name: 'Chocolate mousse',
+      nbPerson: 6,
+      time: 35,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Dessert',
+      vegan: false,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 9,
+      name: 'Mayonnaise',
+      nbPerson: 4,
+      time: 30,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Sauce',
+      vegan: false,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 10,
+      name: 'Mustard',
+      nbPerson: 4,
+      time: 30,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Sauce',
+      vegan: false,
+      instructions: 'Lorem Ispum... '
+    },
+    {
+      id: 11,
+      name: 'Brookie',
+      nbPerson: 6,
+      time: 50,
+      photo: `${this.baseUrl}/apple-pie.jpg`,
+      category: 'Dessert',
+      vegan: false,
+      instructions: 'Lorem Ispum... '
+    },
+  ];
 }
