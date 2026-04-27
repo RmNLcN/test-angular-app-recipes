c00148c813fcaf7ccd43e9733814fb8cc4dd433d
RmNLcN <romainluciano.cda.pro@gmail.com>
2026-04-27 11:45:58 +0200

    Add properties to Recipe Type interface

--- src/app/recipetype.ts
+++ src/app/recipetype.ts
@@ -1,3 +1,10 @@
 export interface RecipeTypeInfos {
-    
+    id: number;
+    name: string;
+    nbPerson: number;
+    time: number;
+    photo: string;
+    type: string;
+    vegan: boolean;
+    instruction: string;
 }
